c5cd05b8bf5ada7f65af498806f851988dffb9e3
ruihuang <haidu.java@gmail.com>
2013-07-03 19:16:43 +0800

    code start

---
+++ source/whalecar-service/src/main/java/com/whalecar/persistence/CarBrandMapper.java
@@ -0,0 +1,11 @@
+package com.whalecar.persistence;
+
+import java.util.List;
+
+import com.whalecar.domain.CarBrand;
+
+public interface CarBrandMapper{
+	
+	public List<CarBrand> getAllBrand();
+
+}
--- whalecar-facade/test.js
+++
@@ -1,7 +0,0 @@
-var soap = require('soap');
-var url = 'http://www.webxml.com.cn/WebServices/WeatherWebService.asmx?wsdl';
-soap.createClient(url, function(err, client) {
-	console.log(err);
-	console.log(client);
-	//client.testString('a');
-});
