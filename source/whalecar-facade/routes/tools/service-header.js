79705839c6b9c373be7c30d13ffa26b453205845
frankskywalker <frankskywalker@gmail.com>
2013-08-16 11:24:13 +0800

    8.16 11.23am

--- source/whalecar-facade/routes/tools/service-header.js
+++ source/whalecar-facade/routes/tools/service-header.js
@@ -2,6 +2,6 @@ var restify = require("restify");
 
 // create json restful service client
 exports.client = restify.createJsonClient({
-	url : 'http://localhost:8080',
+	url : 'http://121.199.52.168:8080',
 	version : '*'
 });
