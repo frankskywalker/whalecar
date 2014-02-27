b40dc9c530b4610bf9a02f32769b98ee3589ad7f
ruihuang <frankskywalker>
2014-02-27 14:03:29 +0800

    网站后台三个页面

---
+++ source/whalecar-facade/routes/manager.js
@@ -0,0 +1,21 @@
+var service = require("./tools/service-header");
+var moment = require('moment');
+
+exports.userOffTicket = function(req,res){
+    service.client.get("/manager/queryUserOffTicket",function(sErr,sReq,sRes,sData){
+        res.render("managerUserOffTicket",{data:sData,moment:moment});
+    });
+}
+
+exports.userOrder = function(req,res){
+    service.client.get("/manager/queryUserOrder",function(sErr,sReq,sRes,sData){
+        res.render("managerUserOrder",{data:sData,moment:moment});
+    });
+}
+
+
+exports.userSubmitPrice = function(req,res){
+    service.client.get("/manager/queryUserSubmitPrice",function(sErr,sReq,sRes,sData){
+        res.render("managerUserSubmitPrice",{data:sData,moment:moment});
+    });
+}
