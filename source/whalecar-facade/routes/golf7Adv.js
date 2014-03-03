efce74d4aeb77092f02e73aa15508b79432aea4c
ruihuang <seabane@msn.com>
2014-03-03 23:47:53 +0800

    golf 7 adver

--- source/whalecar-facade/routes/golf7Adv.js
+++ source/whalecar-facade/routes/golf7Adv.js
@@ -10,13 +10,15 @@ exports.page = function(req,res){
 exports.save = function(req,res,next){
     var golf7Adv = req.body;
     var attentionModelStr = "";
-    for(var i = 0;i < golf7Adv.attentionModel.length;i++){
-        attentionModelStr += golf7Adv.attentionModel[i];
-        if(i != golf7Adv.attentionModel.length - 1){
-            attentionModelStr += ",";
+    if(golf7Adv.attentionModel){
+        for(var i = 0;i < golf7Adv.attentionModel.length;i++){
+            attentionModelStr += golf7Adv.attentionModel[i];
+            if(i != golf7Adv.attentionModel.length - 1){
+                attentionModelStr += ",";
+            }
         }
+        golf7Adv.attentionModel = attentionModelStr;
     }
-    golf7Adv.attentionModel = attentionModelStr;
     service.client.post("/saveGolf7Adv",golf7Adv,function(error){
         if(error){
             next(error);
