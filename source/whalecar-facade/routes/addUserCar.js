33ff07b9624890dbf6c77d4309abec59bb58a492
frankskywalker <Whalefin403>
2014-03-26 11:18:45 +0800

    UserCar

--- source/whalecar-facade/routes/addUserCar.js
+++ source/whalecar-facade/routes/addUserCar.js
@@ -12,7 +12,7 @@ exports.addUserCar = function(req, res, next){
     var carName =idAndCname.split(",")[1];
     <!--userName:userName 前一个是数据库字段，后者是var的对象，/addUserCar后台对应方法名-->
     service.client.post("/addUserCar",{userName:userName,phoneNum:phoneNum,createDate:createDate,carID:carID,carName:carName},function(error, request, response, data){
-        //跳转页面
+        <!--跳转页面-->
         res.redirect("/index");
     });
 }
